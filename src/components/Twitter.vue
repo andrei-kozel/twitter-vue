683e31518b2f68e5b19d1df37f7da65272c34c78
andrei <andrei@pop-os.localdomain>
2020-06-09 19:39:13 +0200

    unit testing

--- src/components/Twitter.vue
+++ src/components/Twitter.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="w-full">
-    <form class="mx-auto bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4 w-1/2 mt-8">
+    <form
+      class="mx-auto bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4 w-1/2 mt-8"
+    >
       <div class="mb-4 flex flex-col">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Tweet something:</label>
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username"
+          >Tweet something:</label
+        >
         <div class="flex">
           <input
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -16,22 +20,37 @@
             :class="error != null ? 'cursor-not-allowed opacity-50' : ''"
             @click.prevent="addTweet"
             @keyup.enter="addTweet"
-          >Tweet</button>
+          >
+            Tweet
+          </button>
         </div>
-        <p class="text-gray-600 text-xs italic mt-1 text-red-600" v-if="error">{{ error }}</p>
+        <p
+          class="error text-gray-600 text-xs italic mt-1 text-red-600"
+          v-if="error"
+        >
+          {{ error }}
+        </p>
         <p
           class="text-gray-600 text-xs italic mt-1"
           :class="[limit >= 400 ? 'text-red-600' : '']"
-        >{{ limit }}/400</p>
+        >
+          {{ limit }}/400
+        </p>
       </div>
     </form>
 
-    <transition-group v-on:enter="enter" tag="div" class="flex flex-col" v-bind:css="false">
+    <transition-group
+      v-on:enter="enter"
+      tag="div"
+      class="flex flex-col"
+      v-bind:css="false"
+    >
       <span
         class="tweet mx-auto w-1/2 mt-6 bg-blue-500 bg-opacity-25 shadow-xl rounded px-8 pt-6 pb-8"
         v-for="(tw, index) in tweets"
         :key="index"
-      >{{ tw }}</span>
+        >{{ tw }}</span
+      >
     </transition-group>
   </div>
 </template>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,41 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount } from "@vue/test-utils";
+import Twitter from "@/components/Twitter";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("Twitter", () => {
+  const wrapper = mount(Twitter);
+  wrapper.setData({ tweet: " ".repeat(444), tweets: ["Test tweet"] });
+
+  it("Show error", () => {
+    expect(wrapper.find(".error").exists()).toBe(true);
+  });
+
+  it("Render tweet", () => {
+    expect(wrapper.find(".tweet").exists()).toBe(true);
+  });
+
+  it("Tweet message", () => {
+    expect(wrapper.vm.$data.tweet).toBe(" ".repeat(444));
+  });
+
+  it("Add tweet by pressing enter button", () => {
+    const wrapper = mount(Twitter);
+    const input = wrapper.find('input[type="text"]');
+    const button = wrapper.find("button");
+    input.setValue("Test keyup trigger 1");
+    button.trigger("keyup.enter");
+    input.setValue("Test keyup trigger 2");
+    button.trigger("keyup.enter");
+    expect(wrapper.vm.$data.tweets.length).toBe(2);
+  });
+
+  it("Add tweet with mouse", () => {
+    const wrapper = mount(Twitter);
+    const input = wrapper.find('input[type="text"]');
+    const button = wrapper.find("button");
+    input.setValue("Test keyup trigger 1");
+    button.trigger("click");
+    input.setValue("Test keyup trigger 2");
+    button.trigger("click");
+    expect(wrapper.vm.$data.tweets.length).toBe(2);
   });
 });
